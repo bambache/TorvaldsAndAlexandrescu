a8fc4a649a2437a66b1dafc619aad94281c843a7
bambache <florin_b_n@yahoo.com>
2014-01-07 22:03:48 +0200

    android course hw1.1

---
+++ gen/com/bambache/torvaldsandalexandrescu/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bambache.torvaldsandalexandrescu;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
